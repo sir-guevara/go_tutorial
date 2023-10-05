e6e80717819832051dcf75ba2c9bc17370b1028a
sir guevara <groovyguev@gmail.com>
2023-10-05 21:15:18 +0100

    king

--- philosophers.go
+++ philosophers.go
@@ -6,45 +6,58 @@ import (
 	"time"
 )
 
-const numPhilosophers = 5
-const numMeals = 3
-
-var chopsticks = make([]*sync.Mutex, numPhilosophers)
-var wg sync.WaitGroup
-
-func main() {
-	for i := 0; i < numPhilosophers; i++ {
-		chopsticks[i] = &sync.Mutex{}
-	}
-
-	for i := 0; i < numPhilosophers; i++ {
-		wg.Add(1)
-		go philosopher(i + 1)
-	}
-
-	wg.Wait()
+type ChopS struct{ sync.Mutex }
+type Philo struct {
+	id              int
+	leftCS, rightCS *ChopS
+	eaten           int
 }
 
-func philosopher(id int) {
-	for meal := 0; meal < numMeals; meal++ {
-		dine(id)
+var wg sync.WaitGroup
+var concurrentEaters int = 0
+var host = make(chan bool, 2)
+
+func (p Philo) eat() {
+	for p.eaten < 3 {
+		host <- true
+
+		p.leftCS.Lock()
+		p.rightCS.Lock()
+
+		fmt.Printf("starting to eat <%d> (%d times)\n", p.id, p.eaten+1)
+		time.Sleep(1 * time.Second)
+		fmt.Printf("finished eating <%d> (%d times)\n", p.id, p.eaten+1)
+		p.eaten = p.eaten + 1
+
+		p.rightCS.Unlock()
+		p.leftCS.Unlock()
+
+		<-host
 	}
 	wg.Done()
 }
 
+func main() {
+	CSticks := make([]*ChopS, 5)
+	for i := 0; i < 5; i++ {
+		CSticks[i] = new(ChopS)
+	}
 
-func dine(id int) {
+	philos := make([]*Philo, 5)
+	for i := 0; i < 5; i++ {
+		philos[i] = &Philo{
+			i + 1,
+			CSticks[i],
+			CSticks[(i+1)%5],
+			0,
+		}
+	}
 
-	left := id - 1
-	right := (id + 1) % numPhilosophers // Corrected the right chopstick index
+	fmt.Printf("philosophers: %v", philos)
 
-	chopsticks[left].Lock()
-	chopsticks[right].Lock()
-
-	fmt.Printf("Philosopher %d is starting to eat\n", id)
-	time.Sleep(time.Millisecond * time.Duration(id*200))
-	fmt.Printf("Philosopher %d is finishing eating\n", id)
-
-	chopsticks[left].Unlock()
-	chopsticks[right].Unlock()
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go philos[i].eat()
+	}
+	wg.Wait()
 }
